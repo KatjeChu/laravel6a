644b9ff54c9634deb199118fc413933f03b07f3b
KatjeChu <58587823+KatjeChu@users.noreply.github.com>
2019-12-23 15:12:49 +0300

    Radiobutton + DB
    Добавила чтение из БД данных из радиобаттона

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    public function show($id){
+        $product = Product::find($id);
+        return view('product-page',['product'=>$product]);
+
+    }
+    public function index(){
+        $products = Product::latest()->get();
+        return view('all-products',['products'=>$products]);
+    }
+
+    public function create(){
+        // $products = Product::latest()->get();
+        return view('product-new');
+    }
+
+    public function store(){
+        request()->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'price'=>'required',
+            'label'=>'required'
+        ]);
+
+        // dump(request()->all());
+        $product = new Product();
+        $product -> title = request('title');
+        $product -> description = request('description');
+        $product -> price = request('price');
+        $product -> label = request('label');
+        $product -> save();
+        return redirect ('product-new-success');
+    }
+
+    public function edit($id){
+        // $products = Product::latest()->get();
+        $product = Product::find($id);
+        return view('product-edit',['product'=>$product]);
+    }
+
+    public function update($id){
+        request()->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'price'=>'required',
+            'label'=>'required'
+        ]);
+
+        // $products = Product::latest()->get();
+        $product = Product::find($id);
+        $product -> title = request('title');
+        $product -> description = request('description');
+        $product -> price = request('price');
+        $product -> label = request('label');
+        $product -> save();
+        
+        return redirect ('product-new-success');
+    }
+
+}
---
+++ resources/views/product-new.blade.php
@@ -0,0 +1,70 @@
+@extends('layout')
+@section('content')
+<section class="page-add">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="page-breadcrumb">
+                        <h2>Добавить новый товар<span>.</span></h2>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <img src="/img/add.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <div class="contact-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <form method="POST"  action="/all-products" class="contact-form">
+                    @csrf
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <input type="text" name="title" id="title" placeholder="Название" value="{{old('title')}}">
+                                    @error('title')
+                                    <p style="color: #F00;">{{$errors->first('title')}}</p>
+                                    @enderror
+                                <input type="textarea" rows="10" cols="30" name="description" id="description" placeholder="Описание" value="{{old('description')}}">
+                                    @error('description')
+                                    <p style="color: #F00;">{{$errors->first('description')}}</p>
+                                    @enderror
+                                <input type="text" name="price" id="price" placeholder="Цена"  value="{{old('price')}}">
+                                    @error('price')
+                                    <p style="color: #F00;">{{$errors->first('price')}}</p>
+                                    @enderror
+                                
+
+                                    
+                                <input type="radio" name="label" id="label1" 
+                                value="Распродажа" {{(old('label') == 'Распродажа') ? 'checked' : ''}}>
+                                Распродажа<Br>
+                                <input type="radio" name="label" id="label2" 
+                                value="Новинка" {{(old('label') == 'Новинка') ? 'checked' : ''}}>
+                                Новинка<Br>                                
+                                <input type="radio" name="label" id="label3" 
+                                value="Рекомендуем" {{(old('label') == 'Рекомендуем') ? 'checked' : ''}}>
+                                Рекомендуем</p>
+                                    @error('label')
+                                    <p style="color: #F00;">{{$errors->first('label')}}</p>
+                                    @enderror
+                                 
+                                 
+                                <!-- <input type="image" name="image" placeholder="Изображение">  -->
+                               
+                            </div>
+                            <div class="col-lg-12 text-right">
+                                <button type="submit">Загрузить</button>
+                            </div>
+                        </div>
+                    </form> 
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
